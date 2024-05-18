bcb362a2eeba0814a7e9a4dad80ded336744968b
ogollaclinton <clintonmcogolla@gmail.com>
2024-05-18 17:03:55 +0000

    configuration management and provisionig with puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,12 @@
+file {'/tmp/school' :
+	ensure => 'file',
+	content => 'I love Puppet',
+	owner => 'www-data',
+	group => 'www-data',
+	mode => '0744',
+
+
+
+
+
+}
---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,17 @@
+# Define a Puppet class for installing Flask
+class flask_installation {
+    package { 'python3-pip': # Ensure pip3 is installed
+        ensure => installed,
+    }
+
+    exec { 'install_flask':
+        command     => '/usr/bin/pip3 install Flask==2.1.0',
+        path        => ['/usr/bin'],
+        unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+        require     => Package['python3-pip'],
+    }
+}
+
+# Include the class to ensure installation
+include flask_installation
+
